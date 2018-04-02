dd664ff27bc50e2477cf27bfb61665a248a28650
ricardoavion <ricardo.aviador95@gmail.com>
2018-04-02 00:09:23 -0600

    Tarea de clase

---
+++ app/src/main/java/com/example/avion/acercade/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.avion.acercade;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.p_image);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.foto_perfil);
+        RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
+        roundedBitmapDrawable.setCircular(true);
+        imageView.setImageDrawable(roundedBitmapDrawable);
+
+    }
+}
