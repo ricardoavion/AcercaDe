b0a207b759900e2267c655ac496b3cedf1edae25
ricardoavion <ricardo.aviador95@gmail.com>
2018-04-07 09:58:40 -0600

    Implementando intent

--- app/src/main/java/com/example/avion/acercade/MainActivity.java
+++ app/src/main/java/com/example/avion/acercade/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.avion.acercade;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    Button share;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,17 @@ public class MainActivity extends AppCompatActivity {
         RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
         roundedBitmapDrawable.setCircular(true);
         imageView.setImageDrawable(roundedBitmapDrawable);
+        share = (Button)findViewById(R.id.btn_share);
+        share.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+
+            }
+
+
+        });
+
 
     }
 }
